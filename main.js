974ef954bb902f84736ed5154f92aa60cf3f6cc9
marc0sfernades <marcosvini1979.mv@gmail.com>
2023-04-29 10:05:01 -0300

    funcionando logica

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ for(i=0; i < 12; i++){
     const randomIndex = Math.floor(Math.random() * array1.length);
     const randomSrc = array1[randomIndex];
     const img = document.createElement("img")
-    img.classList.add("visivel")
     const card=document.createElement("div");
     card.classList.add("card");
-    card.appendChild(img)
+    card.appendChild(img);
+    card.addEventListener('click', function() {
+      checkarSrc(this)
+    });
     caixaPrincipal.appendChild(card);
     document.body.appendChild(caixaPrincipal);
     img.src =randomSrc;
@@ -21,7 +23,7 @@ function adicionarClasse(div) {
     div.addEventListener('click', function(event) {
         const img = event.target.querySelector("img")
         if (img.classList.contains('virado')) {
-            img.classList.remove('virado');
+            img.classList.replace('virado', 'normal');
           } else {
             img.classList.add('virado');
           }
@@ -31,3 +33,39 @@ const divs = document.querySelectorAll('.card');
 divs.forEach(adicionarClasse);
 
 //
+
+var srcAnterior = '';
+var elementoAnrterior;
+let hasDuplicate = false;
+
+function checkarSrc(ele) {
+  
+  if (srcAnterior == '') {
+    srcAnterior = ele.getElementsByTagName('img')[0].currentSrc;
+    console.log(srcAnterior)
+    elementoAnrterior = ele;
+    return;
+  }else if (srcAnterior == ele.getElementsByTagName('img')[0].currentSrc) {	
+    ele.getElementsByTagName('img')[0].src = '';
+    elementoAnrterior.getElementsByTagName('img')[0].src = '';
+    ele.classList.add("Certo");
+    elementoAnrterior.classList.add("Certo");
+    srcAnterior = '';
+    elementoAnrterior = '';
+    setTimeout(function() {
+      var imagens = document.getElementsByTagName("img");
+      for (var i = 0; i < imagens.length; i++) {
+        imagens[i].classList.remove("virado");
+      }
+    }, 50);;
+  }else {
+    srcAnterior= '';
+    setTimeout(function() {
+      var imagens = document.getElementsByTagName("img");
+      for (var i = 0; i < imagens.length; i++) {
+        imagens[i].classList.remove("virado");
+      }
+    }, 250);;
+  }
+
+}
