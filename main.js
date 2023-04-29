a16fd61dfecea57a6f44df6988e1f157553d04c7
marc0sfernades <marcosvini1979.mv@gmail.com>
2023-04-29 12:39:48 -0300

    adicionado o contador de tempo e alguns estilos

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const caixaPrincipal= document.createElement("div");
 caixaPrincipal.classList.add("container");
+const caixaEstilizavel= document.createElement("div");
+caixaEstilizavel.classList.add("caixa_estilo");
+const titulo= document.createElement("h1")
+var texto  = document.createTextNode("Memory Card Game");
+const timerDisplayer= document.createElement("div")
+timerDisplayer.setAttribute("id", "timerDisplay")
+titulo.appendChild(texto);
+caixaEstilizavel.appendChild(titulo)
+caixaEstilizavel.appendChild(timerDisplayer);
 
 const array1 = new Array ("img/img6.png","img/img5.png","img/img4.png", "img/img3.png", "img/img2.png", "img/img1.png","img/img6.png","img/img5.png","img/img4.png", "img/img3.png", "img/img2.png", "img/img1.png");
 
@@ -14,19 +23,16 @@ for(i=0; i < 12; i++){
       checkarSrc(this)
     });
     caixaPrincipal.appendChild(card);
-    document.body.appendChild(caixaPrincipal);
+    caixaEstilizavel.appendChild(caixaPrincipal)
+    document.body.appendChild(caixaEstilizavel);
     img.src =randomSrc;
     array1.splice(randomIndex, 1);
 }
 
 function adicionarClasse(div) {
     div.addEventListener('click', function(event) {
-        const img = event.target.querySelector("img")
-        if (img.classList.contains('virado')) {
-            img.classList.replace('virado', 'normal');
-          } else {
-            img.classList.add('virado');
-          }
+      const img = event.target.querySelector("img")
+      img.classList.add('virado');
     });
 }
 const divs = document.querySelectorAll('.card');
@@ -37,29 +43,32 @@ divs.forEach(adicionarClasse);
 var srcAnterior = '';
 var elementoAnrterior;
 let hasDuplicate = false;
+var ultimoClique = null;
 
 function checkarSrc(ele) {
   
   if (srcAnterior == '') {
     srcAnterior = ele.getElementsByTagName('img')[0].currentSrc;
-    console.log(srcAnterior)
     elementoAnrterior = ele;
+    ultimoClique= ele
     return;
-  }else if (srcAnterior == ele.getElementsByTagName('img')[0].currentSrc) {	
+  }else if (srcAnterior == ele.getElementsByTagName('img')[0].currentSrc && ele !== ultimoClique) {	
     ele.getElementsByTagName('img')[0].src = '';
     elementoAnrterior.getElementsByTagName('img')[0].src = '';
     ele.classList.add("Certo");
     elementoAnrterior.classList.add("Certo");
     srcAnterior = '';
     elementoAnrterior = '';
+    ultimoClique= null;
     setTimeout(function() {
       var imagens = document.getElementsByTagName("img");
       for (var i = 0; i < imagens.length; i++) {
         imagens[i].classList.remove("virado");
       }
-    }, 50);;
+    }, 1);;
   }else {
     srcAnterior= '';
+    ultimoClique= ele;
     setTimeout(function() {
       var imagens = document.getElementsByTagName("img");
       for (var i = 0; i < imagens.length; i++) {
@@ -68,4 +77,25 @@ function checkarSrc(ele) {
     }, 250);;
   }
 
+}
+//
+const timerDisplay = document.getElementById("timerDisplay");
+let startTime;
+let isRunning = false;
+
+document.addEventListener("click", () => {
+  if (!isRunning) {
+    startTime = Date.now();
+    setInterval(updateTimer, 1000);
+    isRunning = true;
+  }
+});
+
+function updateTimer() {
+  const elapsedTime = Date.now() - startTime;
+  const seconds = Math.floor(elapsedTime / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  timerDisplay.innerHTML = display;
 }
